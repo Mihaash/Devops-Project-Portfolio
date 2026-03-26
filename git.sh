22eb85769a694a22d23c0dd48f37921e03e3849b
mihaash <mihaashdharan68@gmail.com>
2026-03-26 10:06:19 +0530

    testing

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@ echo "Enter commit message: "
 read message
 git add .
 git commit -m "$message"
-if git push; then
+if git push -u origin main; then
     echo "Done"
 else
     echo "😂❌ Oops! Git said NO... Try again bro!"
